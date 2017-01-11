7a2da55de990cf94db383b6f4ef78503dd2ddfd2
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-01-11 16:44:26 +0000

    More code

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-var start = {
-  0: {0: true, 1: true},
-  1: {0: true, 1: true}
+function Coord(x, y) {
+  this.x = x;
+  this.y = y;
 }
 
-function keys (obj) {
-  return Object.keys(obj);
+Coord.prototype.toString = function() {
+  return `x${this.x}y${this.y}`;
 }
 
-function getNeighbours (x, y) {
-    return [
-      {x: x-1, y: y-1},
-      {x: x-1, y:   y},
-      {x: x-1, y: y+1},
-      {x:   x, y: y-1},
-      {x:   x, y: y+1},
-      {x: x+1, y: y-1},
-      {x: x+1, y:   y},
-      {x: x+1, y: y+1},
-    ]
-}
-
-function getCells (world) {
-  var cells = [];
-
-  keys(world).forEach(
-    x => keys(world[x]).forEach(
-      y => {
-        cells.push({x, y});
-      }
-    )
-  )
-
-  return cells;
-}
-
-function ifExists (cell, world) {
-  return world[cell.x] && world[cell.x][cell.y];
+function getNeighbours (coord) {
+  var {x, y} = coord;
+  var neighbours = [
+    {x: x-1, y: y-1},
+    {x:   x, y: y-1},
+    {x: x+1, y: y-1},
+    {x: x-1, y:   y},
+    {x: x+1, y:   y},
+    {x: x-1, y: y+1},
+    {x:   x, y: y+1},
+    {x: x+1, y: y+1}
+  ];
+  return neighbours.map(item => new Coord(item.x, item.y));
 }
 
 function getIntermediate (world) {
-  var intermediate = {};
-
-  getCells(world).forEach(cell => {
-    ifExists(cell);
-  });
+  var intermediateWorld = {};
+  Object.keys(world).forEach(
+    key => {
+      let coord = world[key];
+      intermediateWorld[coord] = intermediateWorld[coord] || { count: 0, coord };
+      intermediateWorld[coord].alive = true;
+      let neighbours = getNeighbours(coord);
+      neighbours.forEach(
+        neighbourCoord => {
+          intermediateWorld[neighbourCoord] = intermediateWorld[neighbourCoord] || { count: 0 };
+          intermediateWorld[neighbourCoord].count++;
+        }
+      )
+    }
+  )
+  return intermediateWorld;
 }
 
-getIntermediate(start);
+function getNextTick (intermediateWorld) {
+  var nextTick = {};
+  Object.keys(intermediateWorld)
+}
+
+module.exports = {Coord, getNeighbours};
